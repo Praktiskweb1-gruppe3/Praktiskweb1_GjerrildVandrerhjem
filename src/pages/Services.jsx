3760ee315087502356837fa385814ac666e45dc4
JoPed <jonasbjoern@gmail.com>
2023-01-16 18:00:23 +0100

    Service cards almost done. Same with Resturant section.
    Some text is not styled before merge into main branch. Also there still needs responsive implementation.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,20 +1,29 @@
 import '../sass/Services.scss';
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import UseTranslator from '../hooks/UseTranslator';
 
+import parse from 'html-react-parser';
+
 const Services = () => {
 
     const { filteredData, error, loading } = UseTranslator( "Services", true );
 
-    const largeImgPath = "./assets/images/big/"
-    const mediumImgPath = "./assets/images/medium/"
-    const smallImgPath = "./assets/images/small/"
+    const { filteredData: filteredDataResturant, error: errorResturant, loading: loadingResturant } = UseTranslator( 'Restutant' );
+
+    const largeImgPath = "./assets/images/big/";
+    const mediumImgPath = "./assets/images/medium/";
+    const smallImgPath = "./assets/images/small/";
+
+    useEffect( () => {
+
+        document.querySelector( '#root' ).style.backgroundColor = '#FAFAFF';
+    }, [] )
 
 
     return (
@@ -23,33 +32,36 @@ const Services = () => {
                 filteredData &&
 
                 <Container fluid="lg" >
-                    <h1>{ filteredData[ 0 ].fields.Service_Name }</h1>
-                    <Row className='servicesRow '>
+                    <Row className='servicesRow'>
+                        <h1>{ filteredData[ 0 ].fields.Service_Name }</h1>
                         {
-                            filteredData.slice( 1 ).map( ( service, index ) => (
+                            filteredData.slice( 1 ).map( service => (
                                 <Col
                                     key={ service.id }
                                     xs={ 12 }
-                                    lg={6 }
+                                    lg={ 6 }
                                 >
                                     <div className='services__card'>
                                         <picture>
-                                        <source media="(max-width: 575px)" srcSet={ smallImgPath + 'services/' + service.fields.Image_Name[ 0 ] } />
+                                            <source media="(max-width: 575px)" srcSet={ smallImgPath + service.fields.Image_Name[ 0 ] } />
+                                            <source media="(max-width: 991px)" srcSet={ mediumImgPath + service.fields.Image_Name[ 0 ] } />
+                                            <source media="(min-width: 992px)" srcSet={ largeImgPath + service.fields.Image_Name[ 0 ] } />
 
-                                        <source media="(max-width: 991px)" srcSet={ mediumImgPath + 'services/' + service.fields.Image_Name[ 0 ] } />
+                                            <img
+                                                style={ { width: '100%', height: 'auto', display: 'block' } }
+                                                src={ largeImgPath + service.fields.Image_Name[ 0 ] }
+                                                alt={ service.fields.Image_Description }
+                                            />
 
-                                        <source media="(min-width: 992px)" srcSet={ largeImgPath + 'services/' + service.fields.Image_Name[ 0 ] } />
+                                        </picture>
 
-                                        <img style={ { width: '100%', height: 'auto', display: 'block' } } src={ largeImgPath + service.fields.Image_Name[ 0 ] } alt={ service.fields.Image_Description } />
-                                    </picture>
-
-                                    <div className="card__body">
-                                        <h3 className='card__title'>{ service.fields.Service_Name }</h3>
-                                        <p className='card__description'>{ service.fields.Service_Description }</p>
+                                        <div className="card__body">
+                                            <h3 className='card__title'>{ service.fields.Service_Name }</h3>
+                                            <div className='card__description'>{ parse( service.fields.Service_Description ) }</div>
+                                        </div>
                                     </div>
-                                    </div>
 
-                                    
+
 
                                 </Col>
                             ) )
@@ -58,6 +70,84 @@ const Services = () => {
 
                 </Container>
             }
+
+            <Container fluid="lg" className='resturant'>
+                {
+                    filteredDataResturant &&
+                    <Row >
+                        <Col lg={ 6 }>
+                            <h1>{ filteredDataResturant[ 0 ].fields.Titel }</h1>
+
+                            <Row>
+                                <Col lg={{span:10, offset: 2}}>
+                                    <div className='mainText'
+                                        style={ { fontSize: '2rem' } }>
+
+                                        { parse( filteredDataResturant[ 0 ].fields.Text ) }
+
+                                    </div>
+                                </Col>
+                            </Row>
+
+                        </Col>
+
+                        <Col lg={ {span: 4} }>
+
+                            <figure className='resturant_big_image'>
+                                <picture>
+                                <source media="(max-width: 575px)" srcSet={ smallImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 2 ] } />
+                                <source media="(max-width: 991px)" srcSet={ mediumImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 2 ] } />
+                                <source media="(min-width: 992px)" srcSet={ largeImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 2 ] } />
+
+                                <img
+                                    style={ { width: '100%', height: 'auto', display: 'block' } }
+                                    src={ largeImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 2 ] }
+                                    alt={ filteredDataResturant[ 0 ].fields.Image_Description[ 2 ] }
+                                />
+
+                            </picture>
+                            </figure>
+
+                            
+
+                            <Row>
+                                <Col lg={ 6 }>
+                                    <picture>
+                                        <source media="(max-width: 575px)" srcSet={ smallImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 1 ] } />
+                                        <source media="(max-width: 991px)" srcSet={ mediumImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 1 ] } />
+                                        <source media="(min-width: 992px)" srcSet={ largeImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 1 ] } />
+
+                                        <img
+                                            style={ { width: '100%', height: 'auto', display: 'block' } }
+                                            src={ largeImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 1 ] }
+                                            alt={ filteredDataResturant[ 0 ].fields.Image_Description[ 1 ] }
+                                        />
+
+                                    </picture>
+                                </Col>
+
+                                <Col lg={ 6 }>
+                                    <picture>
+                                        <source media="(max-width: 575px)" srcSet={ smallImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 0 ] } />
+                                        <source media="(max-width: 991px)" srcSet={ mediumImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 0 ] } />
+                                        <source media="(min-width: 992px)" srcSet={ largeImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 0] } />
+
+                                        <img
+                                            style={ { width: '100%', height: 'auto', display: 'block' } }
+                                            src={ largeImgPath + filteredDataResturant[ 0 ].fields.Image_Name[ 0 ] }
+                                            alt={ filteredDataResturant[ 0 ].fields.Image_Description[ 0 ] }
+                                        />
+
+                                    </picture>
+
+                                </Col>
+                            </Row>
+
+                        </Col>
+
+                    </Row>
+                }
+            </Container>
 
         </Container>
 
