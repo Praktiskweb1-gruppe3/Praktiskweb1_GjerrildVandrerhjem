9eff9ea71f97e5e4e3cf1eab7978530bccb681fb
JoPed <jonasbjoern@gmail.com>
2023-01-18 14:45:50 +0100

    Started

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Container} from 'react-bootstrap/Container';
+import {Row}  from 'react-bootstrap/Row';
+
+import UseTranslator from '../hooks/UseTranslator';
+
+
+const About = () => {
+
+    const {filteredData, error, loading} = UseTranslator('About')
+  return (
+
+<></>
+
+  )
+}
+
+export default About
