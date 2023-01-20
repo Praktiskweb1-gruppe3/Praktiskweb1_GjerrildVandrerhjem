389689842ac634061625b5a463342930b774ca57
JoPed <jonasbjoern@gmail.com>
2023-01-20 12:52:18 +0100

    fase 1 done

--- src/pages/Admin/Activities/AdminActivities.jsx
+++ src/pages/Admin/Activities/AdminActivities.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,6 +7,11 @@ import UseTranslator from '../../../hooks/UseTranslator';
 import AdminTitle from '../../../component/Admin/AdminTitle';
 import PostActivities from './PostActivities';
 
+
+import { useGetData } from '../../../hooks/useGetData';
+
+import { Context } from '../../../Context/Context';
+
 const AdminActivities = () => {
 
 
@@ -14,22 +19,34 @@ const AdminActivities = () => {
 
     const [ selectedOperation, setSelectedOperation ] = useState();
 
-    const handleSubmit = ( e ) => {
+    const [ activitiesLanguage, setActivitiesLanguage ] = useState();
 
-        e.preventDefault();
+    const { error: errorLanguage, loading: loadingLanguage, data: dataLanguage, getData: getDataLanguage } = useGetData();
 
-    }
+
+    useEffect( () => {
+        getDataLanguage( 'https://api.airtable.com/v0/app0qMLpB7LbMjc7l/Language', {
+            'Authorization': 'Bearer ' + import.meta.env.VITE_AIRTABLEKEY
+        }, {
+            "sort[0][field]": "ISO"
+
+        } );
+    }, [] );
 
     return (
-        <Container fluid className='adminActivities'>
+        <Container fluid="lg" className='adminActivities'>
 
             { loading && <div>Loading...</div> }
             { error && <div>Der skete en fejl.</div> }
 
-            <AdminTitle pageName="aktiviteter" />
+            <Col lg={{span: 6, offset: 1}}>
+                <AdminTitle pageName="aktiviteter" />
 
-            <Row>
-                <Col lg={2} className="mb-5">
+            </Col>
+
+
+            <Row className='mb-5'>
+                <Col lg={ { span: 3, offset: 1 } } className="px-5">
                     <label htmlFor="selectOperation" className='labels'>Vælg om du vil opret, rette eller slette</label>
                     <select
                         onChange={ ( e ) => setSelectedOperation( e.target.value ) }
@@ -44,17 +61,39 @@ const AdminActivities = () => {
                         <option value="DELETE">Slet</option>
                     </select>
                 </Col>
+
+                <Col lg={ 3 } className="px-5">
+                    { dataLanguage &&
+                        <>
+                            <label htmlFor="language" className='labels'>Vælg hvilken sprog aktiviteten skal oprettes</label>
+                            <select
+                                id="language"
+                                className="select"
+                                onChange={ e => setActivitiesLanguage( e.target.value ) }
+                                defaultValue="Vælg et sprog"
+                            >
+                                <option disabled>Vælg et sprog</option>
+                                {
+                                    dataLanguage.records.map( lang => (
+
+                                        <option key={ lang.id } value={ lang.fields.ISO } >{ lang.fields.Name }</option>
+                                    ) )
+                                }
+                            </select>
+
+                        </> }
+
+                </Col>
+
             </Row>
 
+            { filteredData && selectedOperation && activitiesLanguage &&
+                <>
+
+                    { selectedOperation === 'POST' && <PostActivities filteredData={ filteredData } language={ activitiesLanguage } /> }
 
 
-            { filteredData && selectedOperation &&
-                <form onSubmit={ handleSubmit }>
-
-                    {selectedOperation === 'POST' && <PostActivities filteredData={filteredData} />}
-
-
-                </form>
+                </>
             }
 
         </Container>
--- src/pages/Admin/Activities/PostActivities.jsx
+++ src/pages/Admin/Activities/PostActivities.jsx
@@ -1,53 +1,181 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-const PostActivities = ( { filteredData } ) => {
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+
+import { usePostData } from '../../../hooks/usePostData';
+
+
+const PostActivities = ( { filteredData, language } ) => {
 
     const [ quillInput, setQuillInput ] = useState();
+    const [ position, setPosition ] = useState( null )
+
+    const { error, loading, data, postData } = usePostData()
+
+    const GetLatLng = () => {
+
+        const map = useMapEvents( {
+            click ( e ) {
+                setPosition( [ e.latlng.lat, e.latlng.lng ] );
+            }
+        } )
+
+        return (
+            <>
+                {
+                    position &&
+                    <Marker position={ position }>
+
+                    </Marker>
+                }
+            </>
+
+        )
+    }
+
+    const handleSubmit = ( e ) => {
+
+        e.preventDefault();
+
+        const newActivity = {
+
+            "fields": {
+                "Name": "Opplev Djursland",
+                "Description": "Gjerrild Vandrehjem ligger mellom skog og strand på toppen av Djursland, og har alle de beste familievennlige aktivitetene innen kjøreavstand.\n",
+                "Language": [
+                    "recMirybXLVCuQslg"
+                ],
+                "Latitude": 56.406534359689886,
+                "Longitude": 10.926752610159358,
+                "Image": [
+                    "reccAnaa7h2LX7ZbY"
+                ]
+            }
+        }
+
+        postData( '', newActivity, {
+            'Authorization': 'Bearer ' + import.meta.env.VITE_AIRTABLEKEY,
+            'Content-Type': 'application/json'
+        } );
+
+        let formData = new FormData( e.target );
+        formData.append( "Description", quillInput );
+        formData.append( 'Latitude', position[ 0 ] )
+        formData.append( 'Longitude', position[ 1 ] );
+        // formData.append('Language', )
+
+        console.log( e.target.Image.files[ 0 ].name );
+
+
+
+    }
+
 
     return (
-        <>
+        <form onSubmit={ handleSubmit }>
             <Row>
-                <Col lg={ 2 }>
-                    <label className='labels' htmlFor='activitiesName'>Giv aktiviteten et navn</label>
-                    <input
-                        name="Name"
-                        className='inputs'
-                        type="text"
-                        placeholder='Giv et navn'
-                        id="activitiesName"
-                    />
+
+                <Col lg={ { span: 6, offset: 1 } }>
+
+                    <Row>
+
+                        <Row className='mb-5'>
+                            <Col lg={ 6 }>
+                                <label className='labels' htmlFor='activitiesName'>Navn</label>
+                                <input
+                                    name="Name"
+                                    className='inputs'
+                                    type="text"
+                                    placeholder='Giv aktiviteten et navn'
+                                    id="activitiesName"
+                                />
+                            </Col>
+
+                            <Col lg={ 6 }>
+                                <label className='labels' htmlFor='activitiesImage'>Billede</label>
+                                <input
+                                    name="Image"
+                                    className='inputs'
+                                    type="file"
+                                    placeholder='Giv aktiviteten et navn'
+                                    id="activitiesName"
+                                />
+                            </Col>
+
+                        </Row>
+
+                        <Row className='mb-5'>
+                            <Col lg={ 6 }>
+                                <label className='labels' htmlFor='Image_Text'>Billede tekst</label>
+                                <input
+                                    name="Image_Text"
+                                    className='inputs'
+                                    type="text"
+                                    placeholder='Bruges ovenpå billedet'
+                                    id="Image_Text"
+                                />
+                            </Col>
+                            <Col lg={ 6 }>
+                                <label className='labels' htmlFor='Image_Description'>Billedes alt tekst</label>
+                                <input
+                                    name="Image_Description"
+                                    className='inputs'
+                                    type="text"
+                                    placeholder='Skriv noget tekst der kan bruges som alt tekst'
+                                    id="Image_Description"
+                                />
+                            </Col>
+
+                        </Row>
+
+                        <Row>
+                            <Col lg={ 12 }>
+                                <label className='labels' htmlFor='activitiesDesc'>Beskriv aktiviteten</label>
+                                <ReactQuill
+                                    onChange={ setQuillInput }
+                                    theme="snow"
+                                    className='quillInput'
+                                    placeholder='Lav en beskrivelse af aktiviteten'
+                                    name="Description"
+                                />
+                            </Col>
+
+                        </Row>
+
+                    </Row>
+
                 </Col>
 
-                <Col lg={ 2 }>
-                    <label className='labels' htmlFor='activitiesImage'>Giv aktiviteten et billede</label>
-                    <input
-                        name="Image"
-                        className='inputs'
-                        type="file"
-                        placeholder='Giv et navn'
-                        id="activitiesName"
-                    />
-                </Col>
-            </Row>
-
-            <Row className='mt-5'>
                 <Col lg={ 4 }>
-                    <label className='labels' htmlFor='activitiesDesc'>Beskriv aktiviteten</label>
-                    <ReactQuill 
-                    onChange={ setQuillInput } 
-                    theme="snow" 
-                    className='quillInput' 
-                    placeholder='Lav en beskrivelse af aktiviteten'
-                    name="Description"
-                     />
+                    <MapContainer
+                        center={ [ 56.503961730282896, 10.816764100376847 ] }
+                        zoom={ 12 }
+                        id="mapContainer__adminactivities"
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' />
+
+                        <GetLatLng />
+
+                    </MapContainer>
                 </Col>
+
+                <Col lg={ { span: 4, offset: 3 } }>
+                    <button type="submit" className='btn_post'>Opret aktivitet</button>
+                </Col>
+
+
             </Row>
-        </>
+
+
+
+        </form>
 
     )
 }
